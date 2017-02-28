f158fa1edc667976e89b79aa72419deecfadeeeb
Matt Terry <mcterry@sep.com>
2017-02-28 15:30:25 -0500

    Wraps cookbook for 'git' and includes git::windows recipe

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures git-wrapper'
 long_description 'Installs/Configures git-wrapper'
 version '0.1.0'
 
+depends 'git', '~> 6.0.0' 
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
